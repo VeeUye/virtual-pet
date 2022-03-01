4c66dbe511dee29432bc1625f031c658a63f6756
Vee Uye <vee.uye@gmail.com>
2022-03-01 15:38:30 +0000

    refactor of test suite

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,11 +1,11 @@
 const Pet = require("../src/pet");
-const pet = new Pet("Fido");
 
 describe("constructor", () => {
   it("returns an object", () => {
     expect(new Pet()).toBeInstanceOf(Object);
   });
   it("renames pet", () => {
+    const pet = new Pet("Fido");
     expect(pet.name).toBe("Fido");
   });
 });
