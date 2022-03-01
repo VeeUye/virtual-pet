524fbeca3fbed1d8e418e0c75cbb68ec5da91356
Vee Uye <vee.uye@gmail.com>
2022-03-01 16:33:50 +0000

    extended growUp method to increase hunger by 5, and decrease fitness by 3

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,17 +4,32 @@ describe("constructor", () => {
   it("returns an object", () => {
     expect(new Pet()).toBeInstanceOf(Object);
   });
+
   it("renames pet", () => {
     const pet = new Pet("Fido");
     expect(pet.name).toBe("Fido");
   });
+
   it("has an initial age of 0", () => {
     const pet = new Pet("Fido");
     expect(pet.age).toBe(0);
   });
-  it("increments age by 1", () => {
+
+  it("has an initial hunger of 0", () => {
+    const pet = new Pet("Fido");
+    expect(pet.hunger).toBe(0);
+  });
+
+  it("has an intial fitness of 10", () => {
+    const pet = new Pet("Fido");
+    expect(pet.fitness).toBe(10);
+  });
+
+  it("increments age by 1, hunger by 5, decrements fitness by 3", () => {
     const pet = new Pet("Fido");
     pet.growUp();
     expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -5,10 +5,14 @@
 function Pet(petName) {
   this.name = petName;
   this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
 }
 
 Pet.prototype.growUp = function () {
   this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
 };
 
 module.exports = Pet;
