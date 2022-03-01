9ad68fa5e8ba537df084aacb1c255c64a60d1f58
Vee Uye <vee.uye@gmail.com>
2022-03-01 15:35:57 +0000

    first pass at changing the renaming the name property

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,11 @@
 const Pet = require("../src/pet");
+const pet = new Pet("Fido");
 
 describe("constructor", () => {
   it("returns an object", () => {
-    expect(new Pet("Fido")).toBeInstanceOf(Object);
+    expect(new Pet()).toBeInstanceOf(Object);
+  });
+  it("renames pet", () => {
+    expect(pet.name).toBe("Fido");
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,9 @@
-function Pet() {}
+// const Pet = (petname) => {
+//   this.name = petName;
+// };
+
+function Pet(petName) {
+  this.name = petName;
+}
 
 module.exports = Pet;
