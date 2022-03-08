e24164c175d70ca14c85d886c47a6812adf208ed
Vee Uye <vee.uye@gmail.com>
2022-03-08 22:20:08 +0000

    refactored to use class

--- src/pet.js
+++ src/pet.js
@@ -1,23 +1,25 @@
 const initialAge = 0;
 const deathAge = 30;
-const intialHunger = 0;
+
+const initialHunger = 0;
 const deathHunger = 10;
-const intialFitness = 10;
+
+const initialFitness = 10;
 const deathFitness = 0;
+
 const getFitBy = 4;
 const loseFitnessBy = 3;
 const getHungryBy = 5;
 const decreaseHungerBy = 3;
 const getOlderBy = 1;
 
-function Pet(petName) {
-  this.name = petName;
-  this.age = initialAge;
-  this.hunger = intialHunger;
-  this.fitness = intialFitness;
-}
-
-Pet.prototype = {
+class Pet {
+  constructor(petName) {
+    this.name = petName;
+    this.age = initialAge;
+    this.hunger = initialHunger;
+    this.fitness = initialFitness;
+  }
   get isAlive() {
     return (
       this.fitness > deathFitness &&
@@ -25,50 +27,49 @@ Pet.prototype = {
       this.age < deathAge
     );
   }
-};
 
-Pet.prototype.growUp = function () {
-  if (!this.isAlive) {
-    throw new Error("Your pet is no longer alive :(");
+  growUp() {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive :(");
+    }
+    this.age += getOlderBy;
+    this.hunger += getHungryBy;
+    this.fitness < loseFitnessBy
+      ? (this.fitness = deathFitness)
+      : (this.fitness -= loseFitnessBy);
   }
-  this.age += getOlderBy;
-  this.hunger += getHungryBy;
-  this.fitness < loseFitnessBy
-    ? (this.fitness = deathFitness)
-    : (this.fitness -= loseFitnessBy);
-};
 
-//would like to refactor this using Math.min
-Pet.prototype.walk = function () {
-  if (!this.isAlive) {
-    throw new Error("Your pet is no longer alive :(");
+  feed() {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive :(");
+    }
+    this.hunger -= decreaseHungerBy;
   }
-  this.fitness <= intialFitness - getFitBy
-    ? (this.fitness += getFitBy)
-    : (this.fitness = intialFitness);
-};
 
-Pet.prototype.feed = function () {
-  if (!this.isAlive) {
-    throw new Error("Your pet is no longer alive :(");
+  walk() {
+    if (!this.isAlive) {
+      throw new Error("Your pet is no longer alive :(");
+    }
+    this.fitness <= initialFitness - getFitBy
+      ? (this.fitness += getFitBy)
+      : (this.fitness = initialFitness);
   }
-  this.hunger -= decreaseHungerBy;
-};
 
-Pet.prototype.checkUp = function () {
-  if (!this.isAlive) {
-    return "Your pet is no longer alive :(";
+  checkUp() {
+    if (!this.isAlive) {
+      return "Your pet is no longer alive :(";
+    }
+    if (this.fitness <= loseFitnessBy && this.hunger >= decreaseHungerBy) {
+      return "I am hungry AND I need a walk";
+    }
+    if (this.fitness <= loseFitnessBy) {
+      return "I need a walk";
+    }
+    if (this.hunger >= decreaseHungerBy) {
+      return "I am hungry";
+    }
+    return "I feel great!";
   }
-  if (this.fitness <= loseFitnessBy && this.hunger >= decreaseHungerBy) {
-    return "I am hungry AND I need a walk";
-  }
-  if (this.fitness <= loseFitnessBy) {
-    return "I need a walk";
-  }
-  if (this.hunger >= decreaseHungerBy) {
-    return "I am hungry";
-  }
-  return "I feel great!";
-};
+}
 
 module.exports = Pet;
